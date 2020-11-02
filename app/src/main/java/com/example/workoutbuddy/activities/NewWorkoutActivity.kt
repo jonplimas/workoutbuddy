98f40381d16520dc4d04ff03570662f0adec8a2a
bluebayani <baynaiblue@gmail.com>
2020-11-02 12:03:32 -0800

    🚀Added UI Components
    Add workout 1 - name, category, desc
    Add workout 2 - recyclerview, exercise cards
    
    TO-DO: populate recycler view, populate spinner on exercise card.

--- app/src/main/java/com/example/workoutbuddy/activities/NewWorkoutActivity.kt
+++ app/src/main/java/com/example/workoutbuddy/activities/NewWorkoutActivity.kt
@@ -2,11 +2,33 @@ package com.example.workoutbuddy.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.example.workoutbuddy.R
+import kotlinx.android.synthetic.main.activity_new_workout.*
 
-class NewWorkoutActivity : AppCompatActivity() {
+class NewWorkoutActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    var list_of_items = arrayOf("Full body", "Upper Body", "Lower body", "Core")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_workout)
+        spinner1!!.onItemSelectedListener = this
+
+        // Create an ArrayAdapter using a simple spinner layout and languages array
+        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, list_of_items)
+        // Set layout to use when the list of choices appear
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Set Adapter to Spinner
+        spinner1!!.adapter = aa
+    }
+
+    override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
+        // use position to know the selected item
+    }
+
+    override fun onNothingSelected(arg0: AdapterView<*>) {
+
     }
 }
