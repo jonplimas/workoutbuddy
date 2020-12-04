f01fc3ebd5f96d047f9a2ade17a066a1141ae2e7
jonplimas <42780131+jonplimas@users.noreply.github.com>
2020-12-04 15:57:48 -0800

    Update MainActivity.kt

--- app/src/main/java/com/example/workoutbuddy/activities/MainActivity.kt
+++ app/src/main/java/com/example/workoutbuddy/activities/MainActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var name = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
         val myBundle = Bundle()
         myBundle?.putString("uName", uName)
-
+        name = uName
 
         Toast.makeText(this, uName, Toast.LENGTH_LONG).show()
 
@@ -32,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 //        val favoritesFragment = FavoritesFragment()
         val workoutsFragment = HomeFragment()
         val userFragment = UserFragment()
-        userFragment.arguments = myBundle
+        userFragment.arguments?.putString("uName", name)
 
 
         //set HomeFragment as initial view
